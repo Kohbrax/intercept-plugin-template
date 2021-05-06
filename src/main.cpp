dfb1b71182ce0cd043ffdaf3cc62f3419b234908
Tobias Schwarz <ts@un-a.eu>
2021-05-06 12:39:23 +0200

    - freeze example

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <intercept.hpp>
+#include <Windows.h>
+
+static std::atomic<bool> g_running = false;
+std::thread th;
+
+
+DWORD __stdcall test_thread_main() {
+	while (g_running.load()) {
+		printf("Loop!\n");
+		Sleep(500);
+	}
+	printf("Return..!\n");
+	printf("exit..");
+	return 0;
+}
+
+
+void start() {
+	printf("Starting Thread..\n");
+	g_running.store(true);
+	th = std::thread(test_thread_main);
+}
+
+void stop() {
+	g_running.store(false);
+	printf("stop() - joining!\n");
+	th.join();
+	printf("stop() - join done!\n");
+}
 
 
 int intercept::api_version() { //This is required for the plugin to work.
     return INTERCEPT_SDK_API_VERSION;
 }
 
-void intercept::register_interfaces() {
-    
-}
-
 void intercept::pre_start() {
-    
+	AllocConsole();
+	freopen("CONOUT$", "w", stdout);
+	start();
 }
 
-void intercept::pre_init() {
-    intercept::sqf::system_chat("The Intercept template plugin is running!");
+void intercept::handle_unload() {
+	stop();
 }
